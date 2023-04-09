de3d6486c9af8098ba18aefb371cf7475da5fa39
novitangrh <novitasabila70@gmail.com>
2023-04-10 00:59:29 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_1302213030/CovidConfig.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul8_1302213030
+{
+    internal class CovidConfig
+    {
+        public string SatuanSuhu { get; set; }
+        public int BatasHariDeman { get; set; }
+        public string PesanDitolak { get; set; }
+        public string PesanDiterima { get; set; }
+
+        public static CovidConfig LoadConfig()
+        {
+            var file = "covid_config.json";
+            if (!File.Exists(file))
+            {
+                return new CovidConfig
+                {
+                    SatuanSuhu = "celcius",
+                    BatasHariDeman = 14,
+                    PesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                    PesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+                };
+            }
+
+            var json = File.ReadAllText(file);
+            return JsonConvert.DeserializeObject<CovidConfig>(json);
+        }
+
+        public bool inputData(double suhu, int hariDeman)
+        {
+            double suhuCelsius = SatuanSuhu == "celcius" ? suhu : (suhu - 32) * 5 / 9;
+            return suhuCelsius >= 36.5 && suhuCelsius <= 37.5 && hariDeman < BatasHariDeman;
+        }
+
+
+        public void UbahSatuan()
+        {
+            switch (SatuanSuhu)
+            {
+                case "celcius":
+                    SatuanSuhu = "fahrenheit";
+                    break;
+                case "fahrenheit":
+                    SatuanSuhu = "celcius";
+                    break;
+                default:
+                    Console.WriteLine("Satuan suhu tidak valid.");
+                    return;
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Satuan suhu telah diubah ke {SatuanSuhu}");
+        }
+    }
+}
--- tpmodul8_1302213030/Program.cs
+++ tpmodul8_1302213030/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul8_1302213030;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var cc = CovidConfig.LoadConfig();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {cc.SatuanSuhu} ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("");
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hariDeman = Convert.ToInt32(Console.ReadLine());
+
+        if (cc.inputData(suhu, hariDeman))
+        {
+            Console.WriteLine(cc.PesanDiterima);
+        }
+        else
+        {
+            Console.WriteLine(cc.PesanDitolak);
+        }
+        cc.UbahSatuan();
+    }
+}
